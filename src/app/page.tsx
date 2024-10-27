6c792d05e605577647e3ff35234fbc7272bae08f
milinD537 <milindpithadia22@gmail.com>
2024-10-27 17:07:44 +0530

    cleaned the code a bit added a few comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,53 @@
-import Image from "next/image";
+import { Textanimation } from "@/components/textanimation"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	return (
+		<>
+			<div className="min-h-svh p-6 grid place-items-center">
+				<section className="Hero | grid place-items-center w-full h-full rounded-xl px-4">
+					<div>
+						<Textanimation name="how-to layout?" />
+						<p className="lg:text-xl text-center mt-4 opacity-0 -translate-y-full animate-[text-animation_700ms_forwards]">
+							Welcome to the{" "}
+							<code className="bg-white/20 p-1 rounded-md">
+								design replication
+							</code>{" "}
+							seminar by{" "}
+							<span className="underline underline-offset-4">
+								Milind Pithadia
+							</span>
+						</p>
+					</div>
+				</section>
+			</div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+			<div className="min-h-svh grid place-items-center">
+				<div className="card | max-w-screen-md flex items-start gap-4">
+					<div className="accordion-grid | bg-text text-background py-4 px-6 grid grid-rows-[auto_0fr] has-[~#accordion:checked]:grid-rows-[auto_1fr] transition-[grid-template-rows] rounded-md">
+						<h2 className="text-3xl font-semibold">Inspiration</h2>
+						<p className="text-lg overflow-hidden">
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Rem magnam suscipit corporis officia
+							distinctio odio alias, ipsa quis ipsam, qui
+							repellendus inventore ea, necessitatibus delectus?
+						</p>
+					</div>
+					<input
+						type="checkbox"
+						name=""
+						id="accordion"
+						className=""
+					/>
+					{/* can also use radio buttons similarly for multiple cards */}
+					{/* kept visible for demo */}
+					<label
+						htmlFor="accordion"
+						className="accordionClick bg-red-400 px-6 py-3 rounded-md hover:cursor-pointer"
+					>
+						click
+					</label>
+				</div>
+			</div>
+		</>
+	)
 }
---
+++ src/components/textanimation.tsx
@@ -0,0 +1,18 @@
+export function Textanimation({ name }: { name: string }) {
+	return name.split(" ").map((item, index) => (
+		<h1
+			key={index}
+			className="clamp font-bold uppercase text-center leading-none overflow-hidden"
+		>
+			{item.split("").map((item, index) => (
+				<span
+					className={`inline-block translate-y-full opacity-0 animate-[text-animation_700ms_forwards]`}
+					style={{ animationDelay: `${index * 0.05}s` }}
+					key={index}
+				>
+					{item}
+				</span>
+			))}
+		</h1>
+	))
+}
